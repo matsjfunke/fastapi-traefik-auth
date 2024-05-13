71ea5a69b2fe4a881d9beac522b232db121d8fd4
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-05-13 12:39:23 +0200

    inital frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SignUpForm from './components/SignUpForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sign Up Form</h1>
+      </header>
+      <main>
+        <SignUpForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
